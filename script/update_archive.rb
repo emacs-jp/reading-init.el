17f699bcec623419ead10c021652162efbafd1c3
pogin503 <pogin503@gmail.com>
2018-01-21 17:15:27 +0900

    Update update_archive.rb

--- script/update_archive.rb
+++ script/update_archive.rb
@@ -17,13 +17,22 @@ end
 
 initels = ARGV.map do |url|
   url_path_split = url.split('/')
-  {
-    url: url,
-    name: url_path_split[-1],
-    author_name: url_path_split[3],
-    author_url: url_path_split[0..3].join('/'),
-  }
-}
+  if /^https:\/\/github.com/ =~ url
+    {
+      url: url,
+      name: url_path_split[-1],
+      author_name: url_path_split[3],
+      author_url: url_path_split[0..3].join('/'),
+    }
+  else
+    {
+      url: url,
+      name: url_path_split[-1],
+      author_name: url_path_split[4],
+      author_url: url_path_split[0..4].join('/'),
+    }
+  end
+end
 
 # Clone
 root = File.expand_path("#{File.dirname(__FILE__)}/..")
@@ -36,13 +45,19 @@ data = Marshal.load(Marshal.dump(current_data))
 # yyyy-MM-ddTHH:mm:ss+09:00
 held_date = (Date.parse(data[0]['date']) + 7).strftime('%Y-%m-%dT23:00:%S+09:00')
 
+url = if initels[0][:url] == '^https:\/\/github.com/'
+        `./script/bin/run-query head_commit_query #{initels[0][:url]} #{held_date}`
+      else
+        initels[0][:url]
+      end
+
 data.unshift(
   {
     'id' => (data[0]['id'] + 1),
     'date' => "#{(Date.parse(data[0]['date']) + 7)} 23:00",
     'author' => {
       'name' => initels[0][:author_name],
-      'url' => `./script/bin/run-query head_commit_query #{initels[0][:url]} #{held_date}`,
+      'url' => url,
     },
     'log' => {
       'url' => data[0]['log']['url'],
